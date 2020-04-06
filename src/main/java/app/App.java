81cb03e6f7b447c608cd516b6cbea48c310b26bc
thalesdavi <tdsfreitas@sga.pucminas.br>
2020-04-06 18:00:17 -0300

    Implementação do Switch em vez de If

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -15,25 +15,19 @@ public class App
             System.out.println("1 - Modificar prioridade de acesso.");
             System.out.println("2 - Suspender ou retomar processo.");
             System.out.println("3 - Iniciar novo ciclo.");
-            System.out.println("4 - Sair");
-            System.out.println(" ");
+            System.out.println("4 - Sair\n");
             System.out.print("Escolha sua opção: ");
             String lerNum = sc.nextLine();
             opcao = Integer.parseInt(lerNum);
 
-            if (opcao == 1)
+            switch (opcao)
             {
-                
-            }
-
-            if (opcao == 2)
-            {
-                
-            }
-
-            if (opcao == 3)
-            {
-                
+                case 1: 
+                        break;
+                case 2: 
+                        break;
+                case 3: 
+                        break;
             }
 
         }        
