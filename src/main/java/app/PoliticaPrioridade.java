bd77eb9e432449da5409fc31e4547043181cd225
Matheus Duarte <matheusdcarvalho007@gmail.com>
2020-04-06 20:02:42 -0300

    Corrigindo PoliticaPrioridade

--- src/main/java/app/PoliticaPrioridade.java
+++ src/main/java/app/PoliticaPrioridade.java
@@ -6,13 +6,13 @@ public class PoliticaPrioridade{
     public int getPriorityToSubtract(Processo processo){
         if (processo.getExecutionTime() - this.LIMIT_TIME < 0) return 0;
         // 1 segundo
-        if (processo.getExecutionTime() - this.LIMIT_TIME > this.LIMIT_TIME) return -1;
+        if (processo.getExecutionTime() - this.LIMIT_TIME > this.LIMIT_TIME) return 1;
         // 10 segundos
-        if (processo.getExecutionTime() - this.LIMIT_TIME > Math.pow(10, 10)) return -2;
+        if (processo.getExecutionTime() - this.LIMIT_TIME > Math.pow(10, 10)) return 2;
         // 100 segundos
-        if (processo.getExecutionTime() - this.LIMIT_TIME > Math.pow(10, 11)) return -3;
+        if (processo.getExecutionTime() - this.LIMIT_TIME > Math.pow(10, 11)) return 3;
         // se passar de 100 segundos nos removemos o processo da lista
-        return 0;
+        return -1;
     }
 
 }
